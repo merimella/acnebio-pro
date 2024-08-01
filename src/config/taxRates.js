7ba7bce31a1b6cce28cdbcfec653efdebcd68177
Mariateresa Cirillo <87804504+merimella@users.noreply.github.com>
2024-08-01 11:42:48 +0200

    rimosso file

--- src/config/taxRates.js
+++
@@ -1,6 +0,0 @@
-export const TAX_RATES = {
-    standard: 22,  // Aliquota standard in Italia
-    reduced: 10,   // Aliquota ridotta
-    zero: 0,       // Aliquota zero
-    // Aggiungi altre aliquote se necessario
-  };
